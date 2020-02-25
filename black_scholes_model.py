99d913077be3372097e09d3d9653578624b0ff36
adavis2 <ad4905>
2020-02-24 22:41:16 -0500

    Upgraded pip package, installed scipy and numpy libraries.  Completed code for Newtons approximation method.  Testing and implementation of Black Scholes formula next.

--- black_scholes_model.py
+++ black_scholes_model.py
@@ -4,21 +4,23 @@ import scipy.stats as stats
 #1: Newton's Approximation Method for finding Implied Volatility
 #*************************************************************************************************************#
 
-n = norm.pdf
-N = norm.cdf
+n = stats.norm.pdf
+N = stats.norm.cdf
+
 
 def bs_price(cp_flag,S,K,T,r,v,q=0.0):
-    d1 = (log(S/K)+(r+v*v/2.)*T)/(v*sqrt(T))
-    d2 = d1-v*sqrt(T)
+    d1 = (stats.log(S/K)+(r+v*v/2.)*T)/(v*stats.sqrt(T))
+    d2 = d1-v*stats.sqrt(T)
     if cp_flag == 'c':
-        price = S*exp(-q*T)*N(d1)-K*exp(-r*T)*N(d2)
+        price = S*stats.exp(-q*T)*N(d1)-K*stats.exp(-r*T)*N(d2)
     else:
-        price = K*exp(-r*T)*N(-d2)-S*exp(-q*T)*N(-d1)
+        price = K*stats.exp(-r*T)*N(-d2)-S*stats.exp(-q*T)*N(-d1)
     return price
 
+
 def bs_vega(cp_flag,S,K,T,r,v,q=0.0):
-    d1 = (log(S/K)+(r+v*v/2.)*T)/(v*sqrt(T))
-    return S * sqrt(T)*n(d1)
+    d1 = (stats.log(S/K)+(r+v*v/2.)*T)/(v*stats.sqrt(T))
+    return S * stats.sqrt(T)*n(d1)
 
 
 def find_vol(target_value, call_put, S, K, T, r):
