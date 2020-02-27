0a65e3e58d5e26813ef3b785acf57b770bc509a0
adavis2 <ad4905>
2020-02-26 22:13:49 -0500

    Got the Volatility method working but have to fix input fields and convert their types.  Currently have to enter values manually.

--- black_scholes_model.py
+++ black_scholes_model.py
@@ -1,26 +1,58 @@
+from math import log, sqrt, exp
+from decimal import Decimal
 import scipy.stats as stats
+import datetime
 
 #*************************************************************************************************************#
 #1: Newton's Approximation Method for finding Implied Volatility
 #*************************************************************************************************************#
+from scipy.stats import norm
 
-n = stats.norm.pdf
-N = stats.norm.cdf
+# cp = input("Is this a call or put option: ")
+#
+# #Price of the option
+# V_market = Decimal(input("Please enter the price of the option: "))
+#
+# #Strike Price
+# K = Decimal(input("Please enter the strike price: "))
+#
+# #Amount of days until option expires
+# d = datetime.datetime.now()
+# e = input("Please enter the date that the option is set to expire (mm/dd/yyyy): ")
+# e = datetime.datetime.strptime(e, '%m/%d/%Y')
+# T = (datetime.date(d.year,d.month,d.day) - datetime.date(e.year, e.month, e.day)).days / 365
+#
+# #Actual Stock Price
+# S = Decimal(input("Please enter the current stock price of the option's company: "))
+#
+# #Risk-free rate
+# r = Decimal(input("Please enter the risk-free rate, approximtate the risk-free rate by finding the Treasury bill rate "
+#           "that matures near when the option expires: "))
+
+V_market = 17.5
+K = 585
+T = (datetime.date(2014,10,18) - datetime.date(2014,9,8)).days / 365.
+S = 586.08
+r = 0.0002
+cp = 'c' # call option
+
+n = norm.pdf
+N = norm.cdf
 
 
 def bs_price(cp_flag,S,K,T,r,v,q=0.0):
-    d1 = (stats.log(S/K)+(r+v*v/2.)*T)/(v*stats.sqrt(T))
-    d2 = d1-v*stats.sqrt(T)
+    d1 = (log(S/K)+(r+v*v/2.)*T)/(v*sqrt(T))
+    d2 = d1-v*sqrt(T)
     if cp_flag == 'c':
-        price = S*stats.exp(-q*T)*N(d1)-K*stats.exp(-r*T)*N(d2)
+        price = S*exp(-q*T)*N(d1)-K*exp(-r*T)*N(d2)
     else:
-        price = K*stats.exp(-r*T)*N(-d2)-S*stats.exp(-q*T)*N(-d1)
+        price = K*exp(-r*T)*N(-d2)-S*exp(-q*T)*N(-d1)
     return price
 
 
 def bs_vega(cp_flag,S,K,T,r,v,q=0.0):
-    d1 = (stats.log(S/K)+(r+v*v/2.)*T)/(v*stats.sqrt(T))
-    return S * stats.sqrt(T)*n(d1)
+    d1 = (log(S/K)+(r+v*v/2.)*T)/(v*sqrt(T))
+    return S * sqrt(T)*n(d1)
 
 
 def find_vol(target_value, call_put, S, K, T, r):
@@ -43,3 +75,8 @@ def find_vol(target_value, call_put, S, K, T, r):
 
     # value wasn't found, return best guess so far
     return sigma
+
+implied_vol = find_vol(V_market, cp, S, K, T, r)
+
+print("{}%".format(implied_vol * 100))
+
